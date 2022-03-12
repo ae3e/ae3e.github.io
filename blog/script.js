08316660c9817bd07afeef04cf95b2516c65b7f9
ae3e <dev@ae3e.com>
2022-03-12 09:34:19 +0100

    Add activtities

--- blog/script.js
+++ blog/script.js
@@ -22,11 +22,116 @@ marked.setOptions({
     xhtml: false
 });
 
+let runIcon = `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20" height="20""
+viewBox="0 24 24 24" style="enable-background:new 0 0 24 96;" xml:space="preserve">
+<style type="text/css">
+.st1{fill:#2D2D32;}
+</style>
+<g id="Icons_1_">
+<path class="st1" d="M22.8,41.4c-2-1.8-4.1-3.8-4.4-4.1s-1.2-2.9-2.1-5.3c-0.4-2.8-0.9-3.1-1.1-3.3c-0.3-0.2-0.7-0.3-1.1-0.3l0,0
+   c-0.7,0.1-1.3,0.3-1.9,0.7c-0.6,0.5-1.4,0.8-2.1,0.9c-0.3,0-0.7-0.1-1-0.2c-0.6-0.2-0.6-0.4-0.6-1c0-0.6-0.2-1.2-0.7-1.6
+   C7.7,27.1,7.4,27,7.2,27c-1.6,0-3.8,3.6-4.6,5l-1.3,0.9c-0.3,0.2-0.5,0.6-0.4,0.9c0.3,2.5,2,4.2,5.8,5.6c1.1,0.4,2.1,1,2.9,1.8
+   c0.9,0.9,2,1.7,3.1,2.3c1.5,0.9,3.3,1.4,5.1,1.4c2.7,0,5.6-1.1,5.6-2.8C23.2,41.9,23,41.6,22.8,41.4z M17.6,44
+   c-1.6,0-3.2-0.4-4.6-1.2c-3.4-2.1-2.9-3-6.1-4.2c-3.6-1.4-4.9-2.8-5.1-4.8l1.5-1C4.6,30.4,6.5,28,7.2,28c0,0,0.1,0,0.2,0
+   c0.6,0.4-0.3,2,1.5,2.7c0.4,0.2,0.9,0.3,1.3,0.3c1.9,0,3-1.6,4-1.6c0.2,0,0.3,0.1,0.5,0.1c0.4,0.3,0.8,2.7,0.8,2.7l0.3,0.7h-1.3
+   c-0.3,0-0.5,0.2-0.5,0.5S14,34,14.3,34H16l0.4,1h-1.4c-0.3,0-0.5,0.2-0.5,0.5s0.2,0.5,0.5,0.5h1.8l0.4,1h-1.4
+   c-0.3,0-0.5,0.2-0.5,0.5s0.2,0.5,0.5,0.5h1.9c0.7,0.8,4.5,4.2,4.5,4.2C22.2,42.9,20.2,44,17.6,44z"/>
+</g>
+</svg>`
+let swimIcon = `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20" height="20"
+viewBox="0 24 24 24" xml:space="preserve">
+<style type="text/css">
+.st0{fill:#606065;}
+.st1{fill:#2D2D32;}
+.st2{fill:#FFFFFF;}
+</style>
+<g id="Icons_1_">
+<path class="st1" d="M6.5,31.8c1.5,0,2.8-0.9,3.5-2.1c0.7,1.3,2,2.1,3.5,2.1c1.5,0,2.8-0.9,3.5-2.1c0.7,1.3,2,2.1,3.5,2.1
+   c0.8,0,1.6-0.2,2.3-0.7c0.2-0.2,0.3-0.5,0.1-0.7c-0.2-0.2-0.4-0.3-0.7-0.1c-0.5,0.3-1.1,0.5-1.7,0.5c-1.4-0.1-2.6-1.1-3-2.5
+   c-0.1-0.3-0.4-0.4-0.7-0.2c-0.1,0.1-0.2,0.1-0.2,0.2c-0.4,1.4-1.6,2.4-3,2.5c-1.4-0.1-2.6-1.1-3-2.5c-0.1-0.3-0.4-0.4-0.7-0.2
+   c-0.1,0.1-0.2,0.1-0.2,0.2c-0.4,1.4-1.7,2.4-3.1,2.5c-0.6,0-1.2-0.2-1.7-0.5c-0.2-0.2-0.5-0.1-0.7,0.1c-0.2,0.2-0.1,0.5,0.1,0.7
+   c0,0,0,0,0,0C4.8,31.6,5.6,31.8,6.5,31.8z"/>
+<path class="st1" d="M22.2,42.3c-0.5,0.3-1.1,0.5-1.7,0.5c-1.4-0.1-2.6-1.1-3-2.5c-0.1-0.3-0.4-0.4-0.7-0.2
+   c-0.1,0.1-0.2,0.1-0.2,0.2c-0.4,1.4-1.6,2.4-3,2.5c-1.4-0.1-2.6-1.1-3-2.5c-0.1-0.3-0.4-0.4-0.7-0.2c-0.1,0.1-0.2,0.1-0.2,0.2
+   c-0.4,1.4-1.6,2.4-3,2.5c-0.6,0-1.2-0.2-1.7-0.5c-0.2-0.2-0.5-0.1-0.7,0.1S4,42.9,4.2,43.1c0,0,0,0,0,0c0.6,0.4,1.4,0.7,2.2,0.7
+   c1.5,0,2.8-0.9,3.5-2.1c0.7,1.3,2,2.1,3.5,2.1c1.5,0,2.8-0.9,3.5-2.1c0.7,1.3,2,2.1,3.5,2.1c0.8,0,1.6-0.2,2.3-0.7
+   c0.2-0.2,0.3-0.5,0.1-0.7C22.7,42.2,22.4,42.2,22.2,42.3L22.2,42.3z"/>
+<path class="st1" d="M6.9,35.7c0.7,1.3,2,2.1,3.5,2.1c1.5,0,2.8-0.9,3.5-2.1c0.7,1.3,2,2.1,3.5,2.1c0.8,0,1.6-0.2,2.3-0.7
+   c0.2-0.2,0.3-0.5,0.1-0.7c-0.2-0.2-0.4-0.3-0.7-0.1c-0.5,0.3-1.1,0.5-1.7,0.5c-1.4-0.1-2.6-1.1-3-2.5c-0.1-0.3-0.4-0.4-0.7-0.2
+   c-0.1,0.1-0.2,0.1-0.2,0.2c-0.4,1.4-1.6,2.4-3,2.5c-1.4-0.1-2.6-1.1-3-2.5c-0.1-0.3-0.4-0.4-0.7-0.2c-0.1,0.1-0.2,0.1-0.2,0.2
+   c-0.4,1.4-1.6,2.4-3,2.5c-0.6,0-1.2-0.2-1.7-0.5c-0.2-0.2-0.5-0.1-0.7,0.1S1,36.9,1.3,37.1c0,0,0,0,0,0c0.6,0.4,1.4,0.7,2.1,0.7
+   C4.9,37.8,6.2,37,6.9,35.7z"/>
+</g>
+</svg>`
+let rideIcon = `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20" height="20"
+viewBox="0 24 24 24" style="enable-background:new 0 0 24 96;" xml:space="preserve">
+<style type="text/css">
+.st0{fill:#606065;}
+.st1{fill:#2D2D32;}
+.st2{fill:#FFFFFF;}
+.st3{fill:#FC4C01;}
+</style>
+<g id="Icons_1_">
+<path class="st1" d="M19,35.1c-0.6,0-1.1,0.1-1.7,0.3l-2-4.3h2.2c0.3,0,0.5,0.2,0.5,0.5c0,0.2-0.1,0.3-0.2,0.4
+   c-0.2,0.1-0.3,0.4-0.2,0.7s0.4,0.3,0.7,0.2c0.7-0.4,1-1.3,0.5-2c-0.3-0.5-0.8-0.7-1.3-0.7h-3c-0.3,0-0.5,0.2-0.5,0.5
+   c0,0.1,0,0.1,0,0.2l0.8,1.8H9l-1.2-2.5H9c0.3,0,0.5-0.2,0.5-0.5S9.3,29.1,9,29.1H5c-0.3,0-0.5,0.2-0.5,0.5s0.2,0.5,0.5,0.5h1.7
+   l1.4,3l-1.2,2.4c-2.5-1-5.5,0.2-6.5,2.7s0.2,5.5,2.7,6.5s5.5-0.2,6.5-2.7c0.2-0.4,0.3-0.9,0.3-1.4h2c0.1,0,0.2,0,0.2-0.1l0.1,0
+   c0,0,0.1-0.1,0.1-0.1l0,0l0,0l3.2-6.1l0.8,1.6c-2.3,1.5-3,4.5-1.6,6.9s4.5,3,6.9,1.6s3-4.5,1.6-6.9C22.3,35.9,20.7,35,19,35.1z
+    M14.8,33.6L12,38.9l-2.5-5.4H14.8z M5,44c-2.2,0-4-1.8-4-4s1.8-4,4-4c0.5,0,1,0.1,1.4,0.3l-1.8,3.5c-0.1,0.2,0,0.5,0.2,0.7
+   c0.1,0,0.1,0.1,0.2,0.1H9C8.7,42.5,7,44,5,44z M7.3,36.8c0.9,0.6,1.5,1.7,1.7,2.8H5.8L7.3,36.8z M10,39.5c-0.1-1.5-1-2.8-2.2-3.7
+   l0.9-1.7l2.6,5.4H10z M19,44c-2.2,0-4-1.8-4-4c0-1.4,0.7-2.6,1.8-3.4l1.7,3.6c0.1,0.2,0.4,0.3,0.7,0.2c0.2-0.1,0.3-0.4,0.2-0.6
+   l-1.7-3.6c0.4-0.1,0.8-0.2,1.2-0.2c2.2,0,4,1.8,4,4C23,42.2,21.2,44,19,44z"/>
+</g>
+</svg>`
+let exerciseIcon = `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20" height="20"
+viewBox="0 24 24 24" xml:space="preserve">
+<style type="text/css">
+.st0{fill:#606065;}
+.st1{fill:#2D2D32;}
+.st2{fill:#FFFFFF;}
+</style>
+<g id="Icons_1_">
+<path class="st1" d="M19.9,36.1l-2.4-6.7c-0.1-0.2-0.3-0.3-0.5-0.3l0,0c-0.2,0-0.4,0.1-0.5,0.3l-4.1,11.3l-3-7.3
+   c-0.1-0.2-0.3-0.3-0.5-0.3c-0.2,0-0.4,0.1-0.5,0.3l-2.1,5.9l-1.5-3c-0.1-0.2-0.3-0.2-0.5-0.2H2v1h2.2l1.9,3.7
+   c0.1,0.2,0.3,0.3,0.5,0.3c0.2,0,0.4-0.1,0.5-0.3L9,35l3,7.3c0.1,0.2,0.3,0.3,0.5,0.3l0,0c0.2,0,0.4-0.1,0.5-0.3L17,31.1l2,5.7
+   c0.1,0.2,0.3,0.3,0.5,0.3H22v-1H19.9z"/>
+</g>
+</svg>`
 
-async function getPosts() {
+let icons = {
+    Run: runIcon,
+    Ride: rideIcon,
+    Swim: swimIcon,
+    Exercise: exerciseIcon
+}
+
+let formatDuration = sec => {
+    if (sec < 3600) {
+        return Math.round(sec / 60) + "min";
+    } else {
+        let min = Math.round((sec % 3600) / 60);
+        return Math.floor(sec / 3600) + "h" + (min < 10 ? "0" + min : min)
+    }
+}
+
+let formatSpeed = (ms, type) => {
+    let speed = (ms / 1000 * 3600).toFixed(1) + 'km/h'
+    switch (type) {
+        case "Run":
+            speed = Math.floor(1000 / ms / 60) + ":" + Math.round((1000 / ms) % 60) + "/km"
+            break;
+        case "Swim":
+            speed = Math.floor(100 / ms / 60) + ":" + Math.round((100 / ms) % 60) + "/100m"
+            break;
+        default:
+    }
+    return speed;
+}
+
+async function getEvents() {
     let response = null;
 
-    response = await fetch(`https://directus.ae3e.com/items/posts?sort=-date_created&${window.localStorage.getItem('token') ? 'access_token=' + window.localStorage.getItem('token') : ''}`)
+    response = await fetch(`https://directus.ae3e.com/items/events?sort=-start&${window.localStorage.getItem('token') ? 'access_token=' + window.localStorage.getItem('token') : ''}`)
     const data = await response.json();
     return data;
 }
@@ -156,20 +261,20 @@ if (id) { //d0c7bc5cfb6c972e8d801c7a1959214b
         })
     })
 } else {
-    getPosts().then(data => {
+    getEvents().then(data => {
         let code = ''
 
         console.log(data)
-        let posts = data.data
-        posts.forEach(post => {
+        let events = data.data
+        events.forEach(event => {
             let updated = '';
             let updateLabel = '';
 
-            if (post.date_updated && post.date_created.split('T')[0] !== post.date_updated.split('T')[0] &&
-                new Date(post.date_updated).getTime() > new Date().getTime() - 7 * 24 * 3600000) {
+            if (event.end && event.start.split('T')[0] !== event.end.split('T')[0] &&
+                new Date(event.end).getTime() > new Date().getTime() - 7 * 24 * 3600000) {
                 updateLabel = ' Updated'
             }
-            code += `<div class="blog-post-text"><span class="passive">&gt;_</span> <b>${post.date_created.split('T')[0]}</b>${updated} - <a  href="${post.type == 'notebook' ? post.id : '?id=' + post.id}">${post.title}</a>${post.status === "published" ? "" : '<span style="padding: 1px 5px 1px 5px;background-color:#eb8b7a;">Private</span>'}${updateLabel}<br/></div>
+            code += `<div class="blog-post-text"><span class="passive">&gt;_</span> <b>${event.start.split('T')[0]}</b>${updated} - ${event.category === 'post' ? '<a  href="?id=' + event.extra.id + '">' + event.description + '</a>' : icons[event.extra.type] + ' ' + event.description + ' <span style="color:#CCC;font-size:14px"> ' + (event.extra.distance / 1000).toFixed(1) + 'km | ' + formatDuration(event.extra.moving_time) + ' | ' + formatSpeed(event.extra.average_speed, event.extra.type) + (event.extra.total_elevation_gain !== 0 ? ' | ' + Math.round(event.extra.total_elevation_gain) + 'm' : '') + (event.extra.average_heartrate ? ' | ' + Math.round(event.extra.average_heartrate) + 'bpm' : '')}</span>${event.private ? '<span style="padding: 1px 5px 1px 5px;background-color:#eb8b7a;">Private</span>' : ""}${updateLabel}<br/></div>
             `
         })
         //console.log(data)
@@ -191,7 +296,7 @@ async function getWeather() {
     let response = null;
 
     //response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=48.68853&longitude=5.619284&current_weather=true`)
-    response = await fetch(`https://directus.ae3e.com/items/measurements?access_token=blog&limit=4&sort=-time`)
+    response = await fetch(`https://directus.ae3e.com/items/measurements?limit=4&sort=-time`)
     const data = await response.json();
     return data;
 }
